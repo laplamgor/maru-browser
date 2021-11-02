cfa1af2e9f7cc877185eb559b5248f78d77421a0
laplamgor <laplamgor@gmail.com>
2021-11-03 02:06:35 +0800

    Add simple browser

--- app/src/main/java/com/laplamgor/marubrowser/MainActivity.kt
+++ app/src/main/java/com/laplamgor/marubrowser/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.laplamgor.marubrowser
 
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
+import androidx.fragment.app.FragmentActivity
+import org.mozilla.geckoview.GeckoRuntime
+import org.mozilla.geckoview.GeckoSession
+import org.mozilla.geckoview.GeckoView
 
-class MainActivity : WearableActivity() {
+
+class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Enables Always-on
-        setAmbientEnabled()
+        val view: GeckoView = findViewById(R.id.geckoview)
+        val session = GeckoSession()
+        var runtime = GeckoRuntime.create(this)
+        session.open(runtime)
+        view.setSession(session)
+
+//        session.loadUri("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/")
+        session.loadUri("http://ooi.moe/poi")
     }
 }
