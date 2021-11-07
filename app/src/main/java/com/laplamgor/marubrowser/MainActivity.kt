7858bcc6a65af779c89d493ba2d699e32205f829
laplamgor <laplamgor@gmail.com>
2021-11-08 00:19:45 +0800

    Improve zoom behavior and enable remote debugging

--- app/src/main/java/com/laplamgor/marubrowser/MainActivity.kt
+++ app/src/main/java/com/laplamgor/marubrowser/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.laplamgor.marubrowser
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import androidx.fragment.app.FragmentActivity
 import org.mozilla.geckoview.GeckoRuntime
+import org.mozilla.geckoview.GeckoRuntimeSettings
 import org.mozilla.geckoview.GeckoSession
+import org.mozilla.geckoview.GeckoSessionSettings.VIEWPORT_MODE_MOBILE
 import org.mozilla.geckoview.GeckoView
 
 
@@ -15,11 +19,21 @@ class MainActivity : FragmentActivity() {
 
         val view: GeckoView = findViewById(R.id.geckoview)
         val session = GeckoSession()
-        var runtime = GeckoRuntime.create(this)
+        session.settings.viewportMode = VIEWPORT_MODE_MOBILE;
+
+
+        val builder = GeckoRuntimeSettings.Builder().remoteDebuggingEnabled(true)
+
+        var runtime = GeckoRuntime.create(this, builder.build())
+
         session.open(runtime)
         view.setSession(session)
 
 //        session.loadUri("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/")
         session.loadUri("http://ooi.moe/poi")
+
+        view.setOnTouchListener { v: View?, event: MotionEvent ->
+            event.pointerCount > 1
+        }
     }
 }
---
+++ app/src/main/java/com/laplamgor/marubrowser/UnzoomableGeckoView.kt
@@ -0,0 +1,9 @@
+package com.laplamgor.marubrowser
+
+import android.content.Context
+import android.util.AttributeSet
+import org.mozilla.geckoview.GeckoView
+
+class UnzoomableGeckoView(context: Context?, attrs: AttributeSet?) : GeckoView(context, attrs) {
+
+}
