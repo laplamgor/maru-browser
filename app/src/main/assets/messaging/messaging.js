62f1748867e67448c8eb537f3939acb830afc10e
laplamgor <laplamgor@gmail.com>
2021-11-08 23:03:44 +0800

    Add extension POC

---
+++ app/src/main/assets/messaging/messaging.js
@@ -0,0 +1,1 @@
+browser.runtime.sendNativeMessage("browser", { type: "WPAManifest", manifest: { hi: "hhii" } });
--- app/src/main/java/com/laplamgor/marubrowser/MainActivity.kt
+++ app/src/main/java/com/laplamgor/marubrowser/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.laplamgor.marubrowser
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import androidx.annotation.Nullable
 import androidx.fragment.app.FragmentActivity
-import org.mozilla.geckoview.GeckoRuntime
-import org.mozilla.geckoview.GeckoRuntimeSettings
-import org.mozilla.geckoview.GeckoSession
+import org.json.JSONException
+import org.json.JSONObject
+import org.mozilla.geckoview.*
 import org.mozilla.geckoview.GeckoSessionSettings.VIEWPORT_MODE_MOBILE
-import org.mozilla.geckoview.GeckoView
+import org.mozilla.geckoview.WebExtension.MessageDelegate
+import org.mozilla.geckoview.WebExtension.MessageSender
 
 
 class MainActivity : FragmentActivity() {
+    private var runtime: GeckoRuntime? = null
+
+    private val EXTENSION_LOCATION = "resource://android/assets/messaging/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,16 +28,54 @@ class MainActivity : FragmentActivity() {
         session.settings.viewportMode = VIEWPORT_MODE_MOBILE;
 
 
-        val builder = GeckoRuntimeSettings.Builder().remoteDebuggingEnabled(true)
+        if (runtime == null) {
+            val settings = GeckoRuntimeSettings.Builder().remoteDebuggingEnabled(true).build()
+            runtime = GeckoRuntime.create(this, settings)
+        }
 
-        var runtime = GeckoRuntime.create(this, builder.build())
 
-        session.open(runtime)
+        val messageDelegate: MessageDelegate = object : MessageDelegate {
+            @Nullable
+            override fun onMessage(
+                    nativeApp: String,
+                    message: Any,
+                    sender: MessageSender): GeckoResult<Any>? {
+                if (message is JSONObject) {
+                    val json = message
+                    try {
+                        if (json.has("type") && "WPAManifest" == json.getString("type")) {
+                            val manifest = json.getJSONObject("manifest")
+                            Log.d("MessageDelegate", "Found WPA manifest: $manifest")
+                        }
+                    } catch (ex: JSONException) {
+                        Log.e("MessageDelegate", "Invalid manifest", ex)
+                    }
+                }
+                return null
+            }
+        }
+
+        // Make sure the extension is installed
+        runtime!!
+                .webExtensionController
+                .ensureBuiltIn(EXTENSION_LOCATION, "messaging@example.com")
+                .accept( // Set delegate that will receive messages coming from this extension.
+                        { extension ->
+                            session
+                                    .webExtensionController
+                                    .setMessageDelegate(extension!!, messageDelegate, "browser")
+                        }
+                )  // Something bad happened, let's log an error
+                { e -> Log.e("MessageDelegate", "Error registering extension", e) }
+
+        session.open(runtime!!)
         view.setSession(session)
 
 //        session.loadUri("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/")
         session.loadUri("http://ooi.moe/poi")
 
+        // Disable all two finger gesture within the browser
+        // which are reserved for the zoom view
         view.setOnTouchListener { v: View?, event: MotionEvent ->
             event.pointerCount > 1
         }
